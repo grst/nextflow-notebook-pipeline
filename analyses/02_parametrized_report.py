2052a9e265ee91c725b8c731fbdaa1cd2260800a
Gregor Sturm <mail@gregor-sturm.de>
2022-08-03 18:39:14 +0200

    add working example

--- analyses/02_parametrized_report.py
+++ analyses/02_parametrized_report.py
@@ -3,11 +3,14 @@ import pandas as pd
 import seaborn as sns
 
 iris_path = nxfvars.get("iris_path", "../data/iris.csv")
+# implicit variables - always available through nxfvars unless explicitly disabled
+artifact_dir = nxfvars.get("artifact_dir", "/tmp")
+cpus = nxfvars.get("cpus", 1)
 
 iris = pd.read_csv(iris_path)
 
 iris
 
-sns.scatterplot(x="sepal.width", y="petal.length", hue="variety", data=iris)
+ax = sns.scatterplot(x="sepal.width", y="petal.length", hue="variety", data=iris)
 
-
+ax.get_figure().savefig(f"{artifact_dir}/plot.pdf", bbox_inches="tight")
--- main.nf
+++ main.nf
@@ -10,7 +10,10 @@ workflow {
     ch_iris = Channel.fromPath("data/iris.csv")
 
     PARAMETRIZED_REPORT(
-        file("analyses/02_parametrized_report.py"),
+        Channel.value([
+            ["id": "parametrized_report"],
+            file("analyses/02_parametrized_report.py")
+        ]),
         ch_iris.map{ it -> ["iris_path": it.name] },
         ch_iris
     )
